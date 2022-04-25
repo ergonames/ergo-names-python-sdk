c42648613d12c713d91db657b4a60e917a1f0837
Zack Balbin <zack@zackbalbin.com>
2022-04-25 02:33:12 -0700

    split code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ergonames",
-    version="0.2.1",
+    version="0.2.2",
     author="ErgoNames",
     author_email="developer@ergonames.com",
     description="A SDK for resolving ErgoNames.",
