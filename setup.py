cfd357dd53535c305e0373897def9193fd147b7d
Zack Balbin <zack@zackbalbin.com>
2022-07-05 20:36:43 -0700

    update setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    package_dir={"": "ergonames"},
     packages=setuptools.find_packages(where="ergonames"),
     python_requires=">=3.6",
 )
